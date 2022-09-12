d63d5152abdcde9e6853137eb6815285d510d39d
Guilherme Martos <guilhermemartos2005@gmail.com>
2022-09-12 08:50:58 -0300

    Fazendo a tabela "curso" no MySql

---
+++ src/main/resources/db/migration/V001__Criar_Tabela_Curso.sql
@@ -0,0 +1,11 @@
+Create Table Curso(
+    id bigint not null primary key auto_increment,
+    nomecurso varchar (100)
+);
+
+insert into curso(nomecurso) values('Desenvolvimento de Sistemas');
+insert into curso(nomecurso) values('Química');
+insert into curso(nomecurso) values('Administração');
+insert into curso(nomecurso) values('Logística');
+insert into curso(nomecurso) values('Marketing');
+insert into curso(nomecurso) values('Edificação');
