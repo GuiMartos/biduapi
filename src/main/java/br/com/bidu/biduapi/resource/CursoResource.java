51a5f6bb1634ae1bcf2fa6747371bef3aa4e1fc9
Guilherme Martos <guilhermemartos2005@gmail.com>
2022-11-01 09:47:55 -0300

    Começando a configuração do resource

--- src/main/java/br/com/bidu/biduapi/resource/CursoResource.java
+++ src/main/java/br/com/bidu/biduapi/resource/CursoResource.java
@@ -1,9 +1,22 @@
 package br.com.bidu.biduapi.resource;
 
+import br.com.bidu.biduapi.model.Curso;
+import br.com.bidu.biduapi.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("crusos")
 public class CursoResource {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public List<Curso> listarTodosCursos(){
+        return
+    }
+
 }
