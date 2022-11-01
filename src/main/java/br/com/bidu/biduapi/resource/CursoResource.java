fd36d99204fb43b8b0a82898c24be2906ca6ae22
Guilherme Martos <guilhermemartos2005@gmail.com>
2022-11-01 09:36:54 -0300

    Fazendo a requisição do mapeamento

--- src/main/java/br/com/bidu/biduapi/resource/CursoResource.java
+++ src/main/java/br/com/bidu/biduapi/resource/CursoResource.java
@@ -1,7 +1,9 @@
 package br.com.bidu.biduapi.resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("crusos")
 public class CursoResource {
 }
