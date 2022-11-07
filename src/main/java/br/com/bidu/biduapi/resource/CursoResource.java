abba14ab5114cffd96bafaba770774a69b450373
Guilherme Martos <guilhermemartos2005@gmail.com>
2022-11-07 08:40:22 -0300

    Usando o método get
    Para fazer uma espécie de select do BD

--- src/main/java/br/com/bidu/biduapi/resource/CursoResource.java
+++ src/main/java/br/com/bidu/biduapi/resource/CursoResource.java
@@ -3,20 +3,24 @@ package br.com.bidu.biduapi.resource;
 import br.com.bidu.biduapi.model.Curso;
 import br.com.bidu.biduapi.repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("crusos")
+@RequestMapping("cursos")
 public class CursoResource {
 
     @Autowired
     private CursoRepository cursoRepository;
 
+    @GetMapping("/todos")
     public List<Curso> listarTodosCursos(){
-        return
+        return cursoRepository.findAll();
     }
+
+
 
 }
