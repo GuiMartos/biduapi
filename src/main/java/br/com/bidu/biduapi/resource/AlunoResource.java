11ceea6ef2d4d90ce1392f4cad4a0f74e4688c26
Guilherme Martos <guilhermemartos2005@gmail.com>
2022-11-07 08:51:49 -0300

    AlunoResource feito

---
+++ src/main/java/br/com/bidu/biduapi/resource/AlunoResource.java
@@ -0,0 +1,26 @@
+package br.com.bidu.biduapi.resource;
+
+import br.com.bidu.biduapi.model.Aluno;
+import br.com.bidu.biduapi.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("alunos")
+public class AlunoResource {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @GetMapping("/todos")
+    public List<Aluno> listarTodosAlunos(){
+        return alunoRepository.findAll();
+    }
+
+}
+
+
